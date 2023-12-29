dec29f191b167c58333cb98d6197cdfc813bc8af
Daniel Varava <danielvarava@Daniels-MacBook-Pro.local>
2023-12-30 00:05:01 +0200

    Add to cart functionality

--- custom-js/shop.js
+++
@@ -1,9 +0,0 @@
-var addtocart = document.getElementById('addtocart');
-addtocart.addEventListener("click", addToCart);
-
-function addToCart() {
-    var total=localStorage.getItem('checkout');
-    total++;
-    localStorage.setItem('checkout',total);
-    document.querySelector('#checkout').innerHTML=total;
-}
